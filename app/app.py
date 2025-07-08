06135af2db10cc4e3043d5019892cbec9367f468
DragonBacri <vtheulle@gmail.com>
2025-07-08 16:36:26 +0200

    add docker

--- app/app.py
+++ app/app.py
@@ -66,6 +66,10 @@ def predict():
 
     return jsonify({"flower": flower_name})
 
+@app.route("/health", methods=["GET"])
+def health():
+    return jsonify({"status": "healthy"})
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug= True)
